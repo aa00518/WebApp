9f0da34691e32264e3fa1d04d8cc5abc2fb548cd
Joe Banach <jbanach@silgancontainers.com>
2018-12-11 17:02:25 -0600

    Latest.

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -7,15 +7,16 @@ import { MediaMatcher } from '@angular/cdk/layout';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnDestroy {
-  title = 'Web App';
-  mobileQuery: MediaQueryList;
-  fillerNav = Array.from({length: 4}, (_, i) => `Nav Item ${i + 1}`);
-  fillerContent = Array.from({length: 3}, () =>
+  public title: string = 'Web App';
+  public mobileQuery: MediaQueryList;
+  public fillerNav = Array.from({length: 4}, (_, i) => `Nav Item ${i + 1}`);
+  public fillerContent = Array.from({length: 3}, () =>
       `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+  
   private _mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
